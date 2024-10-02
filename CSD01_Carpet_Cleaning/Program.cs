60624eb2b6871f8cfc095b28c9ce55ee5e3e955d
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2024-10-02 12:25:31 +0300

    Add project files.

---
+++ CSD01_Carpet_Cleaning/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSD01_Carpet_Cleaning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
